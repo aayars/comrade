36326b60d9636b9640d61b7af175ee632ccada91
Alex Ayars <aayars@gmail.com>
2017-10-15 10:32:52 -0700

    add sensitive flag

--- comrade/scripts/post_media.py
+++ comrade/scripts/post_media.py
@@ -9,11 +9,13 @@ from twython import Twython
 @click.option("--image", type=click.Path(dir_okay=False), required=True)
 @click.option("--status", type=str, required=True)
 @click.option("--in-reply-to", type=str)
-def main(config, image, status, in_reply_to=None):
+@click.option("--sensitive", is_flag=True, default=False)
+def main(config, image, status, in_reply_to=None, sensitive=False):
     config = json.load(open(config))
 
     client = Twython(config["api_key"], config["api_secret"], config["access_token"], config["access_secret"])
     client.verify_credentials()
 
     response = client.upload_media(media=open(image, 'rb'))
-    client.update_status(status=status, media_ids=[response['media_id']], in_reply_to_status_id=in_reply_to)
+    client.update_status(status=status, media_ids=[response['media_id']], in_reply_to_status_id=in_reply_to,
+                         possibly_sensitive=sensitive)
