e7ea2e770613a3f001b8b843bbf77f67164e4f8d
Alex Ayars <aayars@gmail.com>
2018-12-09 21:48:38 +0000

    actually load config

--- comrade/scripts/stream_toots_new.py
+++ comrade/scripts/stream_toots_new.py
@@ -1,6 +1,7 @@
 # stream_media.py but it's a Mastodon client
 
 import click
+import json
 import time
 
 from mastodon import Mastodon
@@ -13,14 +14,20 @@ from comrade.streamer import Streamer
 @click.option("--callback", type=str, required=True)
 @click.option("--exclude-user", type=str)
 def main(config, callback, exclude_user=None):
-    client = None
+    print("Starting up...")
+
+    cfg = json.load(open(config))
 
     while(True):
+        print("Getting client...")
+
         try:
             client = Mastodon(
                 access_token = cfg["mastodon_token"],
                 api_base_url = cfg["mastodon_instance"],
             )
+
+            print("Have client {}".format(client))
 
             client.stream_user(Streamer(config=config, callback=callback, exclude_user=exclude_user))
 
