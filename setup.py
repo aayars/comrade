beec2d233064f814c7fc688f17871f52dc9b8599
Alex Ayars <aayars@gmail.com>
2019-01-05 19:11:41 +0000

    version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='comrade',
-      version='0.1.1',
+      version='0.2.0',
       description='Comrade is good Mastodon bot. Салюд!',
       author='Alex Ayars',
       author_email='aayars@gmail.com',
