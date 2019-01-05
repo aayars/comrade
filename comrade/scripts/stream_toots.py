12ba5b737fef4996a3db82c6a526ce7294a4d825
Alex Ayars <aayars@gmail.com>
2019-01-05 19:40:10 +0000

    remove correct file

---
+++ comrade/scripts/stream_toots.py
@@ -0,0 +1,35 @@
+# stream_media.py but it's a Mastodon client
+
+import click
+import json
+import os
+import time
+
+from mastodon import Mastodon
+
+
+@click.command()
+@click.option("--config", type=click.Path(dir_okay=False), required=True)
+@click.option("--callback", type=str, required=True)
+@click.option("--cache-dir", type=click.Path(), required=True, help="Status cache dir")
+@click.option("--exclude-user", type=str)
+def main(config, callback, cache_dir, exclude_user=None):
+    cfg = json.load(open(config))
+
+    while(True):
+        try:
+            client = Mastodon(
+                access_token = cfg["mastodon_token"],
+                api_base_url = cfg["mastodon_instance"],
+            )
+
+            os.environ['COMRADE_CACHE'] = cache_dir
+
+            from comrade.streamer import Streamer
+
+            client.stream_user(Streamer(config, client, callback, exclude_user))
+
+        except Exception as e:
+            print(str(e))
+
+            time.sleep(10)
